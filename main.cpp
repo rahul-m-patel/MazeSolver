7c65c5bad48e93166e4dd5dd0285c94797ce3159
Suryakiran Sureshkumar <suryakiranbdsk@gmail.com>
2024-04-14 17:28:49 -0400

    updated main to handle default execution style

--- main.cpp
+++ main.cpp
@@ -6,22 +6,39 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-    if(argc<3){
-        printf("You must provide the size(odd number) of maze and the type of execution s or p\n");
+    // Check if at least the size of the maze is provided
+    if(argc < 2){
+        printf("You must provide the size (odd number) of the maze and optionally the type of execution (s or p).\n");
         exit(0);
     }
+
+    // Parse the maze size from the first argument
     int size = atoi(argv[1]);
-    char execution = argv[2][0];
-    if(size%2==0){
+    // Ensure the maze size is odd
+    if(size % 2 == 0){
         size++;
-        printf("To maintain symmetry, maze size is generally always odd. Creating maze of size %d\n",size);
+        printf("To maintain symmetry, maze size is generally always odd. Creating maze of size %d\n", size);
     }
-    vector<vector<char> > maze(size, vector<char> (size));
-    mazegenerator(maze,size,true);
-    if (execution == 's')
-        sequential(maze,size,true);
-    else if (execution == 'p')
-        parallel(maze,size,true);
-    else
-        printf("choose either s (for sequential) or p (for parallel)\n");
+
+    // Generate the maze
+    vector<vector<char>> maze(size, vector<char>(size));
+    mazegenerator(maze, size, true);
+
+    // Check if the execution type is provided
+    char execution = (argc > 2) ? argv[2][0] : '\0';
+
+    // Execute based on the provided type or default to both
+    if (execution == 's') {
+        sequential(maze, size, true);
+    } else if (execution == 'p') {
+        parallel(maze, size, true);
+    } else {
+        // No valid execution type provided, execute both by default
+        vector<vector<char>> mazeForSequential = maze;
+        vector<vector<char>> mazeForParallel = maze;
+        sequential(mazeForSequential, size, true);
+        parallel(mazeForParallel, size, true);
+    }
+    
+    return 0;
 }
